409eccc2e967fe3630af68333355db9deef3ebe9
biggoron <dan.ringwald12@gmail.com>
2016-08-08 12:38:46 +0200

    changed librarian to document and corpus classes

--- Rakefile
+++ Rakefile
@@ -1,18 +1,5 @@
 namespace :test do |args|
-  def procdir(dir)
-    Dir[ File.join(dir, "**", '*') ].reject do |p|
-      File.directory? p
-    end
-  end
-
-  def exec_test(dir)
-    files = procdir(dir)
-    files.each do |script_name|
-      puts "Executing #{File.basename(script_name)}"
-      require script_name
-    end
-  end
-
+  # Run rake test:*task*
   desc "Triggers the test suite"
   task :all do
     exec_test("./test")
@@ -22,10 +9,25 @@ namespace :test do |args|
   task :TF do
     exec_test("./test/test_term_frequency/")
   end
-
-  desc "Triggers the librarian test suite"
-  task :librarian do
-    exec_test("./test/test_librarian")
-  end
   
+  desc "Triggers the documents/corpuses test suite"
+  task :corpus do
+    exec_test("./test/corpus")
+  end
+
+  def procdir(dir)
+    # Takes source files in directory
+    Dir[ File.join(dir, "**", '*') ].reject do |p|
+      File.directory? p
+    end
+  end
+
+  def exec_test(dir)
+    # Executes all files in directory - used for testing dir
+    files = procdir(dir)
+    files.each do |script_name|
+      puts "Executing #{File.basename(script_name)}"
+      require script_name
+    end
+  end
 end
--- lib/document.rb
+++ lib/document.rb
@@ -1,22 +1,26 @@
 # TODO: There is nothing here yet
+require_relative 'term_frequency'
 Class Document
 # Represent a unit of text. Many documents can form a corpus,
 # another class
 
-  attr_accessor :length, :max, :skip_words, :source, :corpus
+  # @length is the number of words
+  attr_accessor :length, :skip_words, :source, :tfh
   # Constructors
   def initialize()
+    # Initializes an empty document, not very usefull
+    @source = []
 
-    # An empty document, not very usefull
     @length = 0
 
-    # Tf
-    @tf = nil
+    @tfh = TFHash.new()
 
     # Unrelevant words, punctuation etc...
-    @skip_words = ['', '.', ',', '/'] # For example
+    @skip_words_array_memory = ['.', ' ', ',', '/']
+    @skip_words = /\.| |,|\/|\n/ # For example
   end
 
+  # Constructors
   def self.from_file(filepath)
     obj = self.new
     obj.add_file(filepath)
@@ -33,47 +37,64 @@ Class Document
     obj 
   end
 
-  def add_array(array)
-# # Merge an array of words in the Hash
-#   raise ArgumentError, "The argument needs to be an Array" if array.class != Array
-#   array.each do |word|
-#     # TODO: test the skip words thing
-#     next if @skip_words.include?(word)
-#     @length += 1   if @tfh[word.to_s]  == 0
-#     @max += 1      if @tfh[word.to_s]  == @max
-#     @tfh[word.to_s] =  (@tfh[word.to_s] += 1)
-#   end
-#   self
+  def add_array(array, skip_regex=@skip_regex)
+    array = array.join(' ').split(skip_regex)
+    array.each do |w|
+      next if w == ''
+      @source << w
+      @length += 1
+    end
+    @tfh.add_array(array)
+    self
   end
 
-  # TODO: A rollback function to remove an array/string/file
-  # from the hash
-
-  def add_string(text)
-# # Merge a string in the Hash
-# # The regex should be an instance variable, that can be
-# # constructed from array
-#   raise ArgumentError, "The argument needs to be a string" if text.class != String
-#   self.add_array(text.split(' '))
+  def add_string(text, skip_regex = @skip_regex)
+    self.add_array(text.split(skip_regex))
   end
 
-  def add_file(filepath)
-# # Merge a string in the Hash
-#   raise ArgumentError, "The argument needs to be a string" if filepath.class != String
-#   begin
-#     File.foreach(filepath) { |line|  
-#       self.add_string(line)
-#     }
-#   rescue Exception
-#     STDERR.puts "Failed to open \"#{filepath}\""
-#     raise
-#   ensure
-#   end
-#   self
+  def add_file(filepath, skip_regex = @skip_regex)
+    File.foreach(filepath) { |line|  
+      self.add_string(line, skip_regex)
+    }
+    self
   end
 
-  def tf=
-     
+  def skip_words= (array)
+    if array.class.name == "Array"
+      @skip_words_array_memory = array
+      @skip_words = regexify(array)
+    elsif array.class.name = "Regexp"
+      @skip_words_array_memory = nil
+      @skip_words = array
+    else
+      raise ArgumentError.new("Skip words can be built only from regexp or array of words")
+      return false
+    end
+    return true
   end
 
+  def add_skip_word(word)
+    unless @skip_words_array_memory    
+      raise NoMethodError.new("Can't add skip word if skip word list was built from regexp. Try to build it from array of words.")
+      return false
+    end
+    @skip_words_array_memory << word
+
+    self.update_skip_words
+
+    return true
+  end
+
+  
+
+private
+  def update_skip_words
+    temp = []
+    escape = ['.', '/', '*', '^', '$'] # TODO: needs to be more exhaustive
+    @skip_words_array_memory.each do |w|
+      w = '\\' + w if escape.include?(w)
+      temp << w
+    end
+    @skip_words = Regexp.new(temp.join('|'))
+  end
 end
