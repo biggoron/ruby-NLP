dedd328bd66c5b4c3c46af18d344a20b0af734fe
biggoron <dan.ringwald12@gmail.com>
2016-08-31 12:08:51 +0200

    Finished markov chains, supports multiword nodes

--- lib/markov_chain.rb
+++ lib/markov_chain.rb
@@ -10,62 +10,38 @@ class MarkovChain
   # or tokens.
   def initialize
     @nodes = {}
-    @current_node = nil
   end
 
-  # # TODO: adapt to a multiword node
-  # def generate(n, separator = ' ')
-  #     random_word = @nodes.keys.sample
-  #     current_node = @nodes[random_word]
-  #   end
-  #   string = []
-  #   n.times do
-  #     # builds a string by getting to the next node and
-  #     # appending the corresponding word, n times
-  #     # recursively.
-  #     string << current_node.word
-  #     current_node.next
-  #   end
-  #
-  #   # readable format. By default words are concatenated with
-  #   # spaces
-  #   return string.join(separator)
-  # end
-
-  # TODO: adapt to a multiword node
   def add_node(word)
-    raise ArgumentError, "A node with word: '#{word}' already exists!" if @nodes.key?(word)
+    raise ArgumentError, "A node with word: '#{word}' already exists!" if @nodes.key?(word.to_s)
 
     # Each node object is referenced by its corresponding
-    # word. Indexing is efficient.
-    @nodes[word] = Node.new(word)
+    # word. Indexing by string representation is efficient.
+    @nodes[word.to_s] = Node.new(word)
 
     # Enables concatenation of functions by returning self
     self
   end
 
-  # TODO: adapt to a multiword node
   def remove_node(word)
-    raise ArgumentError, "No node with name: '#{word}'" unless @nodes.key?[word]
+    raise ArgumentError, "No node with name: '#{word}'" unless @nodes.key?(word.to_s)
 
-    @nodes[word] = nil
+    @nodes[word.to_s] = nil
 
     self
   end
 
-  # TODO: adapt to a multiword node
   def add_edge(source, dest, w)
     # Checks if both ends of the edge exist
-    raise ArgumentError, 'Source node not found' unless @nodes.key?(source)
-    raise ArgumentError, 'Destination node not found' unless @nodes.key?(dest)
+    raise ArgumentError, 'Source node not found' unless @nodes.key?(source.to_s)
+    raise ArgumentError, 'Destination node not found' unless @nodes.key?(dest.to_s)
 
     # An edge is caracterized by its destination and its weight
-    @nodes[source].add_edge(dest, w)
+    @nodes[source.to_s].add_edge(dest, w)
 
     self
   end
 
-  # TODO: adapt to a multiword node
   def normalize
     # Makes the sum of weight from every node sum up to 1
     @nodes.each do |_, n|
@@ -73,65 +49,54 @@ class MarkovChain
     end
   end
 
-  # TODO: adapt to a multiword node
   def normalize_node(w)
-    raise ArgumentError, 'Node not found' unless @nodes.key?[w]
-
-    @nodes[w].normalize
-  end
-
-  # TODO: Move to Node and adapt to a multiword node
-  def next
-    unless @current_node.normalized?
-      # Node's transition probabilities need to sum up to 1.
-      puts 'node is not normalized'
-      return self
-    end
-    prob_hash = {}
-    @current_node.edges.inject(0) do |total, (n, w)|
-      # Divides the segment from 0 to 1 according to the
-      # transition probability
-      prob_hash[total + w] = n
-      total + w
-    end
-    # Picks a random number between 0 and 1
-    rand_number = rand
-    node = nil
-    value = 2 # Just needs to be greater than 1
-    prob_hash.each do |p, n|
-      # iterates to find which is the node corresponding to
-      # the picked number.
-      if p <= value && rand_number < p
-        node = n
-        value = p
-      end
-    end
-    @current_node = @nodes[node]
-    self
+    raise ArgumentError, 'Node not found' unless @nodes.key?[w.to_s]
+    @nodes[w.to_s].normalize
   end
 end
 
 # A node contains a word and a list of the following possible nodes.
 # This class is used for the Markov chain text model
 class Node
-  attr_accessor :model_length, :words, :edges
+  attr_accessor :edges
+  attr_reader   :words
 
-  def initialize(words, model_length = words.length)
-    raise ArgumentError, "The model length doesn't match the number of words given" if model_length != words.length
+  def initialize(words)
+    raise ArgumentError, "The model length doesn't match the number of words given"  if model_length != words.length
+    raise ArgumentError, 'First argument needs to be a string or an array'           unless words.is_a?(String) or words.is_a?(Array)
+    raise ArgumentError, "Can't handle length-1 array, please use string"            if words.is_a?(Array) and words.length == 1
 
     @words = words
+    @model_length = words.is_a?(String) ? 1 : words.length
     @edges = {}
   end
 
   # An edge consists of a destination node and a probabilistic weight
   def add_edge(dest, w)
     # Checks if the edge already exists
-    raise ArgumentError, 'Edge is already connected' if @edges.key?(dest)
-    raise ArgumentError, "The first argument of add_edge needs to be a Node. Failed to link #{@words} to destination #{dest}" unless dest.is_a?(Node)
+    raise ArgumentError, 'Edge is already connected' if @edges.key?(dest.to_s)
+    raise ArgumentError, "First argument of add_edge needs to be a Node. Failed to link #{@words} to destination #{dest}" unless dest.is_a?(Node)
 
-    @edges[dest] = w
+    @edges[dest.to_s] = w
 
     self
+  end
+
+  def same_as?(another_node)
+    raise ArgumentError, "Same_as takes a Node as an Argument" unless another_node.is_a?(Node)
+
+    # First check if the node has the same kind of information
+    return false if another_node.model_length != @model_length
+    # If it is a single word, compare simply
+    if self.words.is_a?(String)
+      return true if self.words == another_node.words
+    else # Else compare each element of the array of words
+      self.words.each_with_index do |w, i|
+        return false if w != another_node.words[i]
+      end
+    end
+    # If the program get here, the two words array have same length and same items
+    return true
   end
 
   def normalize
