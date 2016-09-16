a6b94f61b238daa111756d08c89d352a59961e40
biggoron <dan.ringwald12@gmail.com>
2016-09-16 09:21:47 +0200

    rubocoped knn.rb

--- lib/knn.rb
+++ lib/knn.rb
@@ -17,9 +17,9 @@ class Knn
     @ref_points ||= [mean(values)]
     # Computes keys
     @tree_precision = 0.0000001
-    entries = values.collect{ |v| [key(v), v] } 
+    entries = values.collect{ |v| [key(v), v] }
     @branching_factor = params[:branching_factor]
-    @branching_factor ||= 15 # TODO: find a default number 
+    @branching_factor ||= 15 # TODO: find a default number
     # Create tree
     @tree = BPlusTree.bulk_load(entries, @branching_factor)
     @stored_entries = values.length if values
@@ -37,7 +37,7 @@ class Knn
   def get_k(value, k)
     # Get the k nearest neighbours to the entry
 
-    raise ArgumentError.new('There is not enough registered points to satisfy the request') if k >= @stored_entries
+    raise ArgumentError, 'There is not enough registered points to satisfy the request' if k >= @stored_entries
 
     d = 0.001
     mem = nil
@@ -46,20 +46,20 @@ class Knn
 
     loop do
       estimate = get_d(value, d)
-      trigger = 10 ** (Math.log10(@tree_precision).floor)
+      power = Math.log10(@tree_precision).floor
+      trigger = 10**power
       return estimate if mem && (d - mem) < trigger
       case estimate.length <=> k
-        when 1
-          high = d
-          d = low ? (d + low) / 2.0 : d * 0.75
-        when -1
-          low = d
-          d = high ? (d + high) / 2.0 : d * 2
-        when 0
-          return estimate
+      when 1
+        high = d
+        d = low ? (d + low) / 2.0 : d * 0.75
+      when -1
+        low = d
+        d = high ? (d + high) / 2.0 : d * 2
+      when 0
+        return estimate
       end
     end
-    
   end
 
   def get_d(value, distance)
@@ -78,7 +78,6 @@ class Knn
     candidates ? candidates : []
   end
 
-
   # should be put in an external helper file
   def key(v)
     # Computes the key of the value v
@@ -88,7 +87,7 @@ class Knn
 
   def get_closer_ref(v)
     # Gets the closer reference and the distance to it
-    index, distance = @ref_points.inject([0, :infinity, -1]) do |mem, p|
+    index, distance = @ref_points.each_with_object([0, :infinity, -1]) do |p, mem|
       mem[2] += 1
       d = dist(p, v)
       next unless mem[1] == :infinity || d < mem[1]
@@ -106,7 +105,7 @@ class Knn
     # (hash etc...)
     total = values.length
     first = Array.new(values[0].length, 0)
-    sum = values.inject(first) do |temp_sum, v| 
+    sum = values.each_with_object(first) do |v, temp_sum|
       v.each_with_index do |e, i|
         temp_sum[i] += e
       end
@@ -123,17 +122,17 @@ class Knn
     # differ by dx/x > x * 10^(-7) asymptotically
     return 0 if distance <= 0
     digits = - Math.log10(@tree_precision).ceil
-    (Math::atan(distance) * 2 / Math::PI).round(digits)
+    (Math.atan(distance) * 2 / Math::PI).round(digits)
   end
-  
+
   def dist(v1, v2)
     # TODO: implement for hashes
-    d, not_used = v1.inject([0, 0]) do |mem, v|
+    d, = v1.each_with_object([0, 0]) do |v, mem|
       mem[0] += (v - v2[mem[1]]) * (v - v2[mem[1]])
       mem[1] += 1
       mem
     end
-    Math::sqrt(d)
+    Math.sqrt(d)
   end
 
   def lazy_compare(v1, v2, distance)
@@ -144,7 +143,7 @@ class Knn
 
     d2 = distance * distance
 
-    v1.inject([0, 0]) do |mem, v|
+    v1.each_with_object([0, 0]) do |v, mem|
       mem[0] += (v - v2[mem[1]]) * (v - v2[mem[1]])
       return false if mem[0] > d2
       mem[1] += 1
